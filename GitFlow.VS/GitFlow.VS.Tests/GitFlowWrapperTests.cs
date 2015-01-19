fa9c1e61c8486e88f7ce215787002308c9cb419d
Jakob Ehn <jakob@ehn.nu>
2015-01-19 13:14:54 +0100

    Initial commit

---
+++ GitFlow.VS/GitFlow.VS.Tests/GitFlowWrapperTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Runtime.Remoting.Contexts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitFlow.VS.Tests
+{
+    [TestClass]
+    public class GitFlowWrapperTests
+    {
+        private string sampleRepoPath;
+        private const string SampleGitRepoName = "SampleGitRepo";
+
+        public GitFlowWrapperTests()
+        {
+            sampleRepoPath = Path.Combine(Directory.GetCurrentDirectory(), SampleGitRepoName);
+        }
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            if (Directory.Exists(SampleGitRepoName))
+            {
+                DeleteReadOnlyDirectory(SampleGitRepoName);
+            }
+
+            ZipFile.ExtractToDirectory("TestData\\SampleGitRepo.zip", ".");
+        }
+
+        [TestMethod]
+        public void MakeSureTestRepoIsCopied()
+        {
+            Assert.IsTrue(File.Exists("TestData\\SampleGitRepo.zip"));
+        }
+
+        [TestMethod]
+        public void MakeSureTestRepoIsExtracted()
+        {
+            Assert.IsTrue(Directory.Exists(SampleGitRepoName));
+        }
+
+        [TestMethod]
+        public void GitFlowInit()
+        {
+            
+            var gf = new GitFlowWrapper();
+            gf.Init(sampleRepoPath, "master", "dev", "feature", "release", "hotfix", "support", "v");
+        }
+
+        [TestMethod]
+        public void GitFlowInitWithInvalidMasterBranchShouldFail()
+        {
+            try
+            {
+                var gf = new GitFlowWrapper();
+                gf.Init(@"C:\Users\jakobe\Source\Repos\GitFlowTest", "master2", "dev", "feature", "release", "hotfix", "support", "v");
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.Message.Contains("master2"));
+            }
+        }
+
+        [TestMethod]
+        public void ParseMasterBranchQuery()
+        {
+            var gf = new GitFlowWrapper();
+            string query = "Branch name for production releases: [dev]";
+            Assert.IsTrue(gf.IsMasterBranchQuery(query));
+        }
+
+        [TestMethod]
+        public void ParseFeatureBranchQuery()
+        {
+            var gf = new GitFlowWrapper();
+            string query = "Feature branches? [feature/]";
+            Assert.IsTrue(gf.IsFeatureBranchQuery(query));
+        }
+
+        [TestMethod]
+        public void ParseVersionTagPrefixQuery()
+        {
+            var gf = new GitFlowWrapper();
+            string query = "Version tag prefix? [v]";
+            Assert.IsTrue(gf.IsVersionTagPrefixQuery(query));
+        }
+
+        [TestMethod]
+        public void ParseVersionTagPrefixQueryWithEmptyTag()
+        {
+            var gf = new GitFlowWrapper();
+            string query = "Version tag prefix? []";
+            Assert.IsTrue(gf.IsVersionTagPrefixQuery(query));
+        }
+
+        [TestMethod]
+        public void ParseHooksAndFiltersQuery()
+        {
+            var gf = new GitFlowWrapper();
+            string query = "Hooks and filters directory? [c:/Users/jakobe/Source/Repos/GitFlowTest/.git/hooks]";
+            Assert.IsTrue(gf.IsHooksAndFiltersQuery(query));
+        }
+
+        //Necessary in order to delete directory containing readonly files
+        public static void DeleteReadOnlyDirectory(string directory)
+        {
+            foreach (var subdirectory in Directory.EnumerateDirectories(directory))
+            {
+                DeleteReadOnlyDirectory(subdirectory);
+            }
+            foreach (var fileName in Directory.EnumerateFiles(directory))
+            {
+                var fileInfo = new FileInfo(fileName) {Attributes = FileAttributes.Normal};
+                fileInfo.Delete();
+            }
+            Directory.Delete(directory);
+        }
+    }
+
+
+}
---
+++ GitFlow.VS/GitFlow.VS/GitFlowWrapper.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GitFlow.VS
+{
+    public class GitFlowWrapper
+    {
+        public static StringBuilder Output = new StringBuilder("");
+        public static StringBuilder Error = new StringBuilder("");
+        private const string GitFlowDefaultValueRegExp = @"\[(.*?)\]";
+
+        public void Init(string repoDirectory, string masterBranch, string developBranch, string featureBranch, string releaseBranch, string hotfixBranch, string supportBranch, string versionTag)
+        {
+            // Start the child process.
+            using (var p = CreateProcess(repoDirectory))
+            {
+                p.Start();
+                p.ErrorDataReceived += OnErrorReceived;
+                p.BeginErrorReadLine();
+                var input = new StringBuilder();
+
+                var sr = p.StandardOutput;
+                while (!sr.EndOfStream)
+                {
+                    var inputChar = (char) sr.Read();
+                    input.Append(inputChar);
+                    if (StringBuilderEndsWith(input, Environment.NewLine))
+                    {
+                        var line = input.ToString();
+                        input = new StringBuilder();
+                        Debug.WriteLine(line);
+                    }
+                    if (IsMasterBranchQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine(masterBranch);
+                        input = new StringBuilder();
+                    }
+                    else if (IsDevelopBranchQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine(developBranch);
+                        input = new StringBuilder();
+                    }
+                    else if (IsFeatureBranchQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine(featureBranch);
+                        input = new StringBuilder();
+                    }
+                    else if (IsReleaseBranchQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine(releaseBranch);
+                        input = new StringBuilder();
+                    }
+                    else if (IsHotfixBranchQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine(hotfixBranch);
+                        input = new StringBuilder();
+                    }
+                    else if (IsSupportBranchQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine(hotfixBranch);
+                        input = new StringBuilder();
+                    }
+                    else if (IsVersionTagPrefixQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine(versionTag);
+                        input = new StringBuilder();
+                    }
+                    else if (IsHooksAndFiltersQuery(input.ToString()))
+                    {
+                        p.StandardInput.WriteLine("");
+                        input = new StringBuilder();
+                    }
+                }
+
+                if (Error != null && Error.Length > 0)
+                {
+                    throw new Exception(Error.ToString());
+                }
+            }
+        }
+
+        private static Process CreateProcess(string repoDirectory)
+        {
+            return new Process
+            {
+                StartInfo =
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    FileName = "git",
+                    Arguments = "flow init -f",
+                    WorkingDirectory = repoDirectory
+                }
+            };
+        }
+
+        private void OnErrorReceived(object sender, DataReceivedEventArgs dataReceivedEventArgs)
+        {
+            Error = new StringBuilder();
+            Error.Append(dataReceivedEventArgs.Data);
+
+        }
+
+        public bool IsMasterBranchQuery(string input)
+        {
+            var regex = new Regex(@"Branch name for production releases: " + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+
+        public bool IsDevelopBranchQuery(string input)
+        {
+            var regex = new Regex(@"Branch name for ""next release"" development: " + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+
+        public bool IsFeatureBranchQuery(string input)
+        {
+            var regex = new Regex(@"Feature branches\? " + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+
+        public bool IsReleaseBranchQuery(string input)
+        {
+            var regex = new Regex(@"Release branches\? " + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+        public bool IsHotfixBranchQuery(string input)
+        {
+            var regex = new Regex(@"Hotfix branches\? " + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+
+        public bool IsSupportBranchQuery(string input)
+        {
+            var regex = new Regex(@"Support branches\? "  + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+
+        public bool IsVersionTagPrefixQuery(string input)
+        {
+            var regex = new Regex(@"Version tag prefix\? " + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+
+        public bool IsHooksAndFiltersQuery(string input)
+        {
+            var regex = new Regex(@"Hooks and filters directory\? " + GitFlowDefaultValueRegExp);
+            return MatchInput(input, regex);
+        }
+
+        private static bool MatchInput(string input, Regex regex)
+        {
+            var match = regex.Match(input);
+            if (match.Success)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool StringBuilderEndsWith(StringBuilder haystack, string needle)
+        {
+            if (haystack.Length == 0)
+                return false;
+
+            var needleLength = needle.Length - 1;
+            var haystackLength = haystack.Length - 1;
+            for (var i = 0; i < needleLength; i++)
+            {
+                if (haystack[haystackLength - i] != needle[needleLength - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
