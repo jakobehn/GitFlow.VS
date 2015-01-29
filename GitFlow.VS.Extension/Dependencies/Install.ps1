689434db12696fdb4f037c9e9160a39cec5b632a
Jakob Ehn <jakob@ehn.nu>
2015-01-29 16:05:40 +0100

    Cleaned up the installscript a bit

--- GitFlow.VS.Extension/Dependencies/Install.ps1
+++ GitFlow.VS.Extension/Dependencies/Install.ps1
@@ -1,9 +1,17 @@
-﻿$installationPath = Split-Path $MyInvocation.MyCommand.Path
+﻿#Copy necessary files from the util-linux package into %programfiles(x86)%\Git\bin
+#Extracts GitFlow repo beneath extension
+#Runs gitflow\contrib\msysgit-install.cmd
+#
+# NOTE:This script must be executed with elevated priviledges
+#
+
+$installationPath = Split-Path $MyInvocation.MyCommand.Path
 $sourceFile = Join-Path  $installationPath "Dependencies.zip"
 $targetFolder = $installationPath
 $binaries = Join-Path $installationPath "binaries"
 $gitFlowFolder = Join-Path $installationPath "gitflow"
 
+#Remove old directories
 if (Test-Path $binaries){
     Write-Host "Removing binaries directory"
 	Remove-Item $binaries -Recurse
@@ -14,38 +22,34 @@ if (Test-Path $gitFlowFolder){
 	Remove-Item $gitFlowFolder -Recurse
 }
 
+#Extract dependencies.zip
 [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
 [System.IO.Compression.ZipFile]::ExtractToDirectory($sourceFile, $targetFolder)
-
-Write-Host "Extracting files"
 $gitLocation = Join-Path ${Env:ProgramFiles(x86)} "Git\bin"
-Sleep 1
 
 Write-Host "Copy binaries to Git installation directory"
 Copy-Item -Path "$binaries\*.*" -Destination "$gitLocation" -Force
 
+#Check if gitflow need to be installed
 if(Test-Path (Join-Path $gitLocation "git-flow"))
 {
     Write-Host "GitFlow already installed"
     exit
 }
-Write-Host "Installing gitflow"
-$installScript = Join-Path $installationPath "gitflow\contrib\msysgit-install.cmd"
 
-#Start-Process -FilePath "$installScript" -Wait -passthru
+#Run gitflow install script
+$installScript = Join-Path $installationPath "gitflow\contrib\msysgit-install.cmd"
 $pinfo = New-Object System.Diagnostics.ProcessStartInfo
 $pinfo.FileName = $installScript
 $pinfo.UseShellExecute = $true
-
 $pinfo.Arguments = """c:\program files (x86)\git"""
 
 $p = New-Object System.Diagnostics.Process
 $p.StartInfo = $pinfo
 $p.Start() | Out-Null
 $p.WaitForExit() 
-Write-Host "exit code: " + $p.ExitCode
 
 Write-Host "Installation done!"
-Sleep 1
+
 
 
