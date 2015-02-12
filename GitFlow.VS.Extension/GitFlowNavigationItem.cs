43ef1d2a418ca464c70c77d0a50ac35a99cf744b
Jakob Ehn <jakob@ehn.nu>
2015-02-12 11:45:33 +0100

    Detecting TFVC or Git. Added background color on hover item

--- GitFlow.VS.Extension/GitFlowNavigationItem.cs
+++ GitFlow.VS.Extension/GitFlowNavigationItem.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.Composition;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.Controls;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TeamFoundation.Git.Extensibility;
 using TeamExplorer.Common;
 
 namespace GitFlowVS.Extension
@@ -9,24 +14,54 @@ namespace GitFlowVS.Extension
     [TeamExplorerNavigationItem("59168500-14BC-4EE9-BB1F-7B2B970A4AF6",1500)]
     public class GitFlowNavigationItem : TeamExplorerBaseNavigationItem
     {
+        private readonly IServiceProvider serviceProvider;
+        private readonly ITeamExplorer teamExplorer;
+        private readonly IGitExt gitService;
+
         [ImportingConstructor]
         public GitFlowNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
             : base(serviceProvider)
         {
+            try
+            {
+                this.serviceProvider = serviceProvider;
+                UpdateVisible();
+                Text = "GitFlow";
+                Image = Resources.LinkIcon;
+                IsVisible = true;
+                teamExplorer = GetService<ITeamExplorer>();
+                gitService = (IGitExt)serviceProvider.GetService(typeof(IGitExt));
+                teamExplorer.PropertyChanged += TeamExplorerOnPropertyChanged;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
 
-            IsVisible = true;
-            Text = "GitFlow";
-            Image = Resources.LinkIcon;
+        protected override void ContextChanged(object sender, ContextChangedEventArgs e)
+        {
+            UpdateVisible();
+            base.ContextChanged(sender, e);
+        }
+
+        private void TeamExplorerOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            UpdateVisible();
+        }
+
+        private void UpdateVisible()
+        {
+            IsVisible = false;
+            if (gitService != null && gitService.ActiveRepositories.Any())
+            {
+                IsVisible = true;
+            }
         }
 
         public override void Execute()
         {
-            var service = GetService<ITeamExplorer>();
-            if (service == null)
-            {
-                return;
-            }
-            service.NavigateToPage(new Guid(GuidList.GitFlowPage), null);
+            teamExplorer.NavigateToPage(new Guid(GuidList.GitFlowPage), null);
         }
     }
 }
