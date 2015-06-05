5d13972103c294701d6147dfd4018f443d7185ed
Jakob Ehn <jakob@ehn.nu>
2015-06-05 15:33:42 +0200

    Make version script use $env:BUILD_BUILDNUMBER directly

--- Scripts/SetVsixVersion.ps1
+++ Scripts/SetVsixVersion.ps1
@@ -1,10 +1,7 @@
-param(
-    [string]$buildVersion
-    )
 
 $paramFiles=get-childitem "*.vsixmanifest" -rec -Verbose
 foreach ($file in $paramFiles)
 {
-    Write-Host -Verbose ("Replacing version in " + $file.PSPath + " with version " + $buildVersion)
-	(Get-Content $file.PSPath) | Foreach-Object { $_ -replace "1.0.0", "$buildVersion" } | Set-Content $file.PSPath
+    Write-Host -Verbose ("Replacing version in " + $file.PSPath + " with version " + $env:BUILD_BUILDNUMBER)
+	(Get-Content $file.PSPath) | Foreach-Object { $_ -replace "1.0.0", "$env:BUILD_BUILDNUMBER" } | Set-Content $file.PSPath
 }
