79b254baca78470ffdc7af148497d133749f9b00
Jakob Ehn <jakob@ehn.nu>
2018-12-08 13:13:42 +0100

    Migrated to VS2019, converted to AsyncPackage

--- GitFlow.VS.Extension/GitFlow.VS.ExtensionPackage.cs
+++ GitFlow.VS.Extension/GitFlow.VS.ExtensionPackage.cs
@@ -1,26 +1,26 @@
 ﻿using System.Runtime.InteropServices;
-using Microsoft.ApplicationInsights;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Settings;
 using TeamExplorer.Common;
+using System;
+using System.Threading;
 
 namespace GitFlowVS.Extension
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
     [Guid(GuidList.GuidGitFlowVsExtensionPkgString)]
-    public sealed class GitFlowVSExtension : Package
+    public sealed class GitFlowVSExtension : AsyncPackage
     {
         public GitFlowVSExtension()
         {
-            
         }
-        protected override void Initialize()
+
+        protected override System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            base.Initialize();
             UserSettings.ServiceProvider = this;
+            return System.Threading.Tasks.Task.FromResult<object>(null);
         }
     }
 }
